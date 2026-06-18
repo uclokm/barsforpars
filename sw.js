7c6b99b98bbfdff34ae347d45f659c014a5c489c
uclokm <153621866+uclokm@users.noreply.github.com>
2026-06-18 10:41:02 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'bfp-v1';
+const ASSETS = ['./', './index.html', './F1.mp3', './golfcart.jpg', './manifest.json', './bg.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
